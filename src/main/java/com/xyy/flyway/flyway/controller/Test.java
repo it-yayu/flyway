9176f3f299157bc4c744f09856c3ec4253d2dc4f
yayu <15735653659@163.com>
2020-09-02 11:17:37 +0800

    测试

---
+++ src/main/java/com/xyy/flyway/flyway/controller/Test.java
@@ -0,0 +1,13 @@
+package com.xyy.flyway.flyway.controller;
+
+/**
+ * @author yayu
+ * @title: Test
+ * @description: TODO
+ * @date 2020/9/211:16
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("htht");
+    }
+}
